134184f0d73a126125e1dbea29ff4504ed21feef
Wayne <neilwang1992@gmail.com>
2017-01-03 14:54:19 -0500

    Delete FeatureNorm.m

--- Perceptron/FeatureNorm.m
+++
@@ -1,8 +0,0 @@
-function NormFeature = FeatureNorm(Features)
-	
-	m = size(Features, 1);
-	Features_std = std(Features, 1);
-	Features_std(find(Features_std==0)) = 1;
-	NormFeature = (Features - repmat(mean(Features,1), m, 1)) ./ repmat(Features_std, m, 1);
-
-end
